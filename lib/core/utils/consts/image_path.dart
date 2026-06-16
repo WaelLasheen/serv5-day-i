d726a5d605e56b0f70f484ee2b4af8b691848275
Wael Lasheen <lasheenwael9@gmail.com>
2026-06-16 14:09:21 +0300

    add google icon

--- lib/core/utils/consts/image_path.dart
+++ lib/core/utils/consts/image_path.dart
@@ -1,4 +1,5 @@
 class ImagePath {
   static const _baseUrl = 'assets/images';
   final String marketingConsulting = '$_baseUrl/Marketing consulting.svg';
+  final String google = '$_baseUrl/google.svg';
 }
